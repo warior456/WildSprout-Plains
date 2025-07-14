7f65066be3d00a098f7f28fe668430e6eba07aab
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-07-15 00:21:06 +0200

    Initial commit

---
+++ src/client/java/net/wildsprout/WildSproutPlainsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wildsprout;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WildSproutPlainsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
